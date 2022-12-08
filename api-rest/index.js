52ba928d0f440ade82900daaafbcf0f94a4bd20c
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-12-08 17:03:15 -0300

    mongoose - instalação de pacote

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,3 +1,38 @@
-console.log("Olá mundo")
-var nome = prompt("Digite seu nome", "")
-alert("O nome" + nome + " foi coletado com sucesso.")
+console.log("Olá mundo");
+
+//Importação de pacotes/módulos para uso da aplicação
+var express = require("express"); //Importação do pacote express
+var app = express(); //Inicialização da aplicação 'app' pelo pacote
+
+//Importação do pacote mongoose
+var mongoose = require("mongoose");
+// Configuração da conexão com o MongoDB no serviço cloud MongoDB Atlas
+const uri =
+  "mongodb+srv://Eliabe:281178ed@senai-mongodb.v8cvxpc.mongodb.net/?retryWrites=true&w=majority";
+
+console.log(uri);
+
+//Validação da configuração da conexão com o MongoDB
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("A conexão com o MongoDB foi realizada com sucesso!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Rota para exibir uma mensagem ao usuário
+app.get("/", (req, res) => {
+  res.send("Olá mundo! Esta é a página inicial da nossa aplicação.");
+});
+
+//A porta é uma variável de ambiente
+//const porta = process.env.PORT || 8080;
+const porta = 8080;
+app.listen(porta, () => {
+  console.log("Servidor inicializado na porta: " + porta);
+});
